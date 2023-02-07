7f945e1acb91929648d057f2b602ed46a43f6f12
harunkumars <harun@betacraft.io>
2023-02-07 22:22:46 +0530

    Initial commit

---
+++ lib/ActiveAdminReports.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "ActiveAdminReports/version"
+
+module ActiveAdminReports
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ActiveAdminReports/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ActiveAdminReports
+  VERSION = "0.1.0"
+end
