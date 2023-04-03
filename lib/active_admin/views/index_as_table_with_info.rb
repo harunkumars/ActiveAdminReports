0fcda99b6f24a8683aad7a4b2b726d9be13279cc
harunkumars <harun@betacraft.io>
2023-04-03 14:43:40 +0530

    redirect report #show to report_runs #index

--- lib/active_admin/views/index_as_table_with_info.rb
+++ lib/active_admin/views/index_as_table_with_info.rb
@@ -5,9 +5,7 @@ module ActiveAdmin
     class IndexAsTableWithInfo < IndexAsTable
       def build(page_presenter, collection)
         @page_presenter = page_presenter
-
         parent_attributes_table
-
         super
       end
 
